9dae24adb4ebf2e9ae57289aa42aae130dcf39b1
April <fazingin@gmail.com>
2019-10-07 16:47:26 -0400

    oops fix some stuff

--- cmds/help.ts
+++ cmds/help.ts
@@ -1,14 +1,14 @@
-const Discord = require('discord.js');
-const prefix = require('../config.json')
-module.exports.run = async(client, message, args) => {
-    let embed1 = new Discord.RichEmbed()
-    .setTitle("FarmCord Help Menu")
-    .setColor("RANDOM")
-    .addField('=>help', 'Help menu')
-    .addField('=>plant', 'Plant a seed')
-    .addField('=>shop', 'show the store')
+import { RichEmbed } from 'discord.js';
+
+module.exports.run = async (client, message, args) => {
+    let embed1 = new RichEmbed()
+        .setTitle("FarmCord Help Menu")
+        .setColor("RANDOM")
+        .addField('=>help', 'Help menu')
+        .addField('=>plant', 'Plant a seed')
+        .addField('=>shop', 'show the store')
     message.author.send(embed1)
 }
-module.exports.client = {
-name: "help"
+module.exports.info = {
+    name: "help"
 }
