ac851ca3175ed9d0417cdd1a9c68542eab33e8f3
DV54321 <49874713+DV54321@users.noreply.github.com>
2019-10-07 17:55:24 -0400

    Add files via upload

--- cmds/help.ts
+++ cmds/help.ts
@@ -5,6 +5,8 @@ module.exports.run = async(client, message, args) => {
     .setTitle("FarmCord Help Menu")
     .setColor("RANDOM")
     .addField('=>help', 'Help menu')
+    .addField('=>plant', 'Plant a seed')
+    .addField('=>shop', 'show the store')
     message.author.send(embed1)
 }
 module.exports.program = {
