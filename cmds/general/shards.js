444062b59f0578fc3ed0c7ab6506d5f032750fc4
DV54321 <49874713+DV54321@users.noreply.github.com>
2019-11-06 12:52:45 -0500

    Update shards.js

--- cmds/general/shards.js
+++ cmds/general/shards.js
@@ -1,7 +1,7 @@
 module.exports.run = async (client, message, args) => {
  let shardsContent = ['STATUS | SHARD | PING | GUILDS'];
-client.ws.shards.forEach(shard => { shardsContent.push(`${shard.status == 0 ? ':green_heart:' : ':broken_heart:' } - ${shard.id} - ${shard.ping}ms - ${client.guilds.size} guilds`)});
-message.channel.send(shardsContent.join('\n'));
+client.ws.shards.forEach(shard => { shardsContent.push(`${shard.status == 0 ? '💚' : '💔' } - ${shard.id} - ${shard.ping}ms - ${client.guilds.size} guilds`)});;
+message.channel.send(`\`\`\`${shardsContent.join('\n')}\`\`\``)
 
 }
 module.exports.info = {
