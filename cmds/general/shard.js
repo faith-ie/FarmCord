19e39394035286e50c253446f398b690abb0d493
DV54321 <49874713+DV54321@users.noreply.github.com>
2019-11-06 10:42:52 -0500

    Add files via upload

--- cmds/general/shard.js
+++ cmds/general/shard.js
@@ -1,8 +1,8 @@
 module.exports.run = async (client, message, args) => {
-    let output = ['SHARD | STATUS | GUILDS', '__________________']
+    let output = ['SHARD | STATUS | GUILDS | PING', '__________________']
 const shards = client.ws.shards.map(s => s.status);
 shards.forEach((alive) => alive === 0 ? '💚' : '💔');
-
+shards.map(sh => `${sh.id}` === currentShard ? `>>>${sh.id}`.length : sh.id.toString().length, client.ws.shard.status === 5 )
 }
 module.exports.info = {
     name: 'shards',
