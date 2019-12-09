756f0d14f1c3b469d9a670c12f1e0012c4d73bb3
DV54321 <49874713+DV54321@users.noreply.github.com>
2019-12-08 19:31:37 -0500

    Name Changing

---
+++ cmds/owner/setname.js
@@ -0,0 +1,8 @@
+const { owner } = require('../../config.json')
+module.exports.run = async (client, message, args) => {
+if(!owner.includes(message.author.id)) return
+client.user.setUsername(args[0])
+}
+module.exports.info = {
+name: 'setname')
+}
