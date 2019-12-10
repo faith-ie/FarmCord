0531cf68be4b942c5b0ff60d12a68a9546f29a0a
DV54321 <danielviola4458@gmail.com>
2019-12-10 13:59:00 -0500

    Fixed

--- cmds/owner/setstatus.js
+++ cmds/owner/setstatus.js
@@ -1,27 +1,11 @@
-<<<<<<< HEAD
 const { owner } = require('../../config.json')
 module.exports.run = async (client, message, args) => {
   if (!owner.includes(message.author.id)) return
-  if (args[0] === 'online') client.user.setStatus('online')
-  if (args[0] === 'idle') client.user.setStatus('idle')
-  if (args[0] === 'dnd') client.user.setStatus('dnd')
-  if (args[0] === 'offline') client.user.setStatus('offline')
-  message.channel.send('👌')
+  if (args[0] === 'online') client.user.setStatus('online').then(message.channel.send('👌'))
+  if (args[0] === 'idle') client.user.setStatus('idle').then(message.channel.send('👌'))
+  if (args[0] === 'dnd') client.user.setStatus('dnd').then(message.channel.send('👌'))
+  if (args[0] === 'offline') client.user.setStatus('offline').then(message.channel.send('👌'))
 }
 module.exports.info = {
   name: 'setstatus'
 }
-=======
-const { owner } = require('../../config.json')
-module.exports.run = async (client, message, args) => {
-  if (!owner.includes(message.author.id)) return
-  if (args[0] === 'online') client.user.setStatus('online')
-  if (args[0] === 'idle') client.user.setStatus('idle')
-  if (args[0] === 'dnd') client.user.setStatus('dnd')
-  if (args[0] === 'offline') client.user.setStatus('offline')
-  message.channel.send('👌')
-}
-module.exports.info = {
-  name: 'setstatus'
-}
->>>>>>> 5e4656a9aa97d61b168586049118eaae5ebeaf0e
