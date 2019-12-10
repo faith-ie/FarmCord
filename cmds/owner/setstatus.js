8760e6a5b79a4ae956b54eee055e99cc41bc7f45
DV54321 <49874713+DV54321@users.noreply.github.com>
2019-12-10 13:56:40 -0500

    Update setstatus.js

--- cmds/owner/setstatus.js
+++ cmds/owner/setstatus.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const { owner } = require('../../config.json')
 module.exports.run = async (client, message, args) => {
   if (!owner.includes(message.author.id)) return
@@ -11,17 +11,4 @@ module.exports.run = async (client, message, args) => {
 module.exports.info = {
   name: 'setstatus'
 }
-=======
-const { owner } = require('../../config.json')
-module.exports.run = async (client, message, args) => {
-  if (!owner.includes(message.author.id)) return
-  if (args[0] === 'online') client.user.setStatus('online')
-  if (args[0] === 'idle') client.user.setStatus('idle')
-  if (args[0] === 'dnd') client.user.setStatus('dnd')
-  if (args[0] === 'offline') client.user.setStatus('offline')
-  message.channel.send('👌')
-}
-module.exports.info = {
-  name: 'setstatus'
-}
->>>>>>> 5e4656a9aa97d61b168586049118eaae5ebeaf0e
+
