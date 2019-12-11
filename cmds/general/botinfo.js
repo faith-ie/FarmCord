56efc795afc8bd14c07aaf3531b42efae7a96214
DV54321 <49874713+DV54321@users.noreply.github.com>
2019-12-11 10:24:07 -0500

    Fixed lol

--- cmds/general/botinfo.js
+++ cmds/general/botinfo.js
@@ -1,11 +1,11 @@
 const mem = Math.ceil(process.memoryUsage().rss)
-const uptime = Math.ceil(process.uptime())
+const uptime = Math.floor(process.uptime())
 const Discord = require('discord.js')
 module.exports.run = async (client, message) => {
   const embed1 = new Discord.MessageEmbed()
     .setTitle('FarmCord Info')
     .setColor('#42f5ce')
-    .addField(`I am using ${mem / 1024 / 1024}MB`, 'Wow! Look at that memory usage!')
+    .addField(`I am using ${mem / 1e+6}MB`, 'Wow! Look at that memory usage!')
     .addField(`and I have been online for ${uptime} minutes`, 'Look at that uptime!')
     .setFooter(`${message.author.name}`)
   message.channel.send(embed1)
