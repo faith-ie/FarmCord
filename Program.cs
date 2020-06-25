542a7c7ebce6599de01f807e919df8559dd7ce24
dvsazei <danielviola4457@gmail.com>
2020-06-25 13:37:51 -0400

    Rewrite in C# because tbh i'd have to rewrite everything and why not do it in another lang

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace FarmCord
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+            _client.Log += Log;
+            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("Discord Token"));
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
