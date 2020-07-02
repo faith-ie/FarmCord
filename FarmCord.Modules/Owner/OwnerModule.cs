7a1acee550f1f1ac9aaec77b73287beca7bb41b8
dvsazei <danielviola4457@gmail.com>
2020-07-02 15:43:14 -0400

    ohhhhh god that took a while

--- FarmCord.Modules/Owner/OwnerModule.cs
+++ FarmCord.Modules/Owner/OwnerModule.cs
@@ -1,14 +1,15 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System.Threading.Tasks;
 
 namespace FarmCord.Owner.Module
 {
+    [RequireOwner]
     public class OwnerModule : ModuleBase
     {
         [Command("userblacklist")]
         [Summary("Blacklists users from using the bot")]
         [Alias("ubl")]
-        [RequireOwner]
         public async Task UserBlackListAsync([Remainder][Summary("Blacklists users from using the bot")] string UserBlackList = "")
         {
             await ReplyAsync("h");
@@ -16,49 +17,46 @@ namespace FarmCord.Owner.Module
         [Command("serverblacklist")]
         [Summary("Blacklists servers from using the bot")]
         [Alias("sbl")]
-        [RequireOwner]
         public async Task ServerBlackListAsync([Remainder][Summary("Blacklists servers from using the bot")] string ServerBlackList = "")
         {
             await ReplyAsync("Test");
         }
         [Command("dm")]
         [Summary("DM's a person")]
-        [RequireOwner]
-        public async Task DmAsync([Remainder][Summary("DM's a person using the bot")] string dm = "")
+        public async Task DmAsync(string user, [Remainder] string dm = "")
         {
-            await ReplyAsync("000000");
+            IUser User = await Context.Client.GetUserAsync(ulong.Parse(user));
+            IDMChannel Dm = await User.GetOrCreateDMChannelAsync();
+            ISelfUser client = Context.Client.CurrentUser;
+            var embed = new EmbedBuilder();
+            embed.WithAuthor($"Owner of {client}");
+            embed.WithThumbnailUrl(client.GetAvatarUrl());
+            embed.WithColor(Color.DarkTeal);
+            embed.WithDescription(dm);
+            embed.WithFooter("You can reply to this message with =>contact");
+            await Dm.SendMessageAsync(embed: embed.Build());
         }
-
         [Command("listservers")]
         [Summary("list all the servers the bot is in")]
-        [RequireOwner]
-
         public async Task ListServersAsync([Remainder][Summary("list all the servers the bot is in")] string listservers = "")
         {
             await ReplyAsync("many servers");
         }
-
         [Command("setgame")]
         [Summary("sets the bot's game")]
-        [RequireOwner]
         public async Task SetGameAsync([Remainder][Summary("Sets the bots game")] string setgame = "")
         {
             await ReplyAsync("gaming");
         }
-
         [Command("setstatus")]
         [Summary("Sets the bots status")]
-        [RequireOwner]
         public async Task SetStatusAsync([Remainder][Summary("sets the bots status")] string setstatus = "")
         {
             await ReplyAsync("0215028");
         }
-
         [Command("shutdown")]
         [Summary("Shuts down the bot")]
         [Alias("die", "kill")]
-        [RequireOwner]
-
         public async Task ShutDownAsync([Remainder][Summary("Shuts down the bot")] string shutdown = "")
         {
             await ReplyAsync("Shutting Down");
